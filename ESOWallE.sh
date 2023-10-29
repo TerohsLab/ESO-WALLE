5b726235a80ee3839173f2b022b8282db93bfe13
Teroh <xian.vizjereij@gmail.com>
2023-10-29 14:44:59 +0100

    Updated to new proprietary Java 8 release

--- ESOWallE.sh
+++ ESOWallE.sh
@@ -14,19 +14,19 @@ URLBaseTTC="https://eu.tamrieltradecentre.com/download/PriceTable"
 # Minion currently only runs under proprietary Java Orcale 8
 # You need to manually download the Java 8 files from here : 
 
-# Current - Stable
-# Version : Java jre1.8.0_371
-# Release date : 25 April 2023
-# Direct : https://javadl.oracle.com/webapps/download/AutoDL?BundleId=248233_ce59cff5c23f4e2eaf4e778a117d4c5b
-# Source : https://www.itechtics.com/java-8-update-371/
-
 # Current - Testing
+# Version : Java jre1.8.0_391
+# Release date : 20 October 2023
+# Direct : https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249192_b291ca3e0c8548b5a51d5a5f50063037
+# Source : https://www.itechtics.com/java-8-update-391/
+
+# Current - Stable
 # Version : Java jre1.8.0_381
 # Release date : 20 July 2023
 # Direct : https://javadl.oracle.com/webapps/download/AutoDL?BundleId=248763_8c876547113c4e4aab3c868e9e0ec572
 # Source : https://www.itechtics.com/java-8-update-381/
 
-PATHBinaryJava="/path/to/Java/jre1.8.0_381/bin/java"
+PATHBinaryJava="/path/to/Java/jre1.8.0_391/bin/java"
 PATHBinaryMinion="/path/to/Minion/Minion-jfx.jar"
 
 ############################################################
